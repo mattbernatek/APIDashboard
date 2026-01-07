9c6fd49bc4f2f7e5382ebf5741874a843331d742
Matt Bernatek <mateusz.bernatek@gmail.com>
2026-01-07 14:34:01 -0500

    Changed the API selection and amended the HTML file to match. Cleaned up in-line JS. JS file now ready for code.

---
+++ app.js
@@ -0,0 +1,64 @@
+const quoteBtn = document.querySelector("#quote-btn");
+const catBtn = document.querySelector("#cat-btn");
+const weatherBtn = document.querySelector("#weather-btn");
+const currencyBtn = document.querySelector("#currency-btn");
+const moviesBtn = document.querySelector("#movies-btn");
+const githubBtn = document.querySelector("#github-btn");
+const jokeBtn = document.querySelector("#joke-btn");
+const boredBtn = document.querySelector("#bored-btn");
+
+const quoteOutput = document.querySelector("#quote-output");
+const catOutput = document.querySelector("#cat-output");
+const weatherOutput = document.querySelector("#weather-output");
+const currencyOutput = document.querySelector("#currency-output");
+const moviesOutput = document.querySelector("#movies-output");
+const githubOutput = document.querySelector("#github-output");
+const jokeOutput = document.querySelector("#joke-output");
+const boredOutput = document.querySelector("#bored-api-output");
+
+const cityInput = document.querySelector("#city-input");
+const amountInput = document.querySelector("#amount-input");
+const sourceCurrencyInput = document.querySelector("#source-currency-input");
+const targetCurrencyInput = document.querySelector("#target-currency-input");
+const githubUserInput = document.querySelector("#github-user-input");
+
+quoteBtn.addEventListener("click", getRandomQuote);
+catBtn.addEventListener("click", getCatImage);
+weatherBtn.addEventListener("click", getWeather);
+currencyBtn.addEventListener("click", getExchange);
+moviesBtn.addEventListener("click", getMovies);
+githubBtn.addEventListener("click", getGitHubUser);
+jokeBtn.addEventListener("click", getJoke);
+boredBtn.addEventListener("click", getBoredActivity);
+
+function getRandomQuote() {
+
+}
+
+function getCatImage() {
+
+}
+
+function getWeather() {
+  
+}
+
+function getExchange() {
+
+}
+
+function getMovies() {
+
+}
+
+function getGitHubUser() {
+  
+}
+
+function getJoke() {
+
+}
+
+function getBoredActivity() {
+
+}
